25338cf3214f1ee7b62bfcb534ef4a83f7f45cec
Dan Morgan <damorgan@hugeinc.com>
2019-04-21 19:29:41 -0400

    Draggable progress

--- src/draggable-slider/slider.jsx
+++ src/draggable-slider/slider.jsx
@@ -1,37 +1,38 @@
 import React from "react";
-import styles from "./slider.module.css";
+import styles from "./slider.module.scss";
 
-export function Slider() {
-  return (
-    <div className={styles.container}>
-      <ul className={styles.carousel}>
-        <li className={styles.card}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo iusto
-          ipsum ratione deleniti amet facilis animi nemo impedit placeat, ab
-          obcaecati magnam explicabo aperiam veniam magni accusantium non
-          tempora enim?
-        </li>
-        <li className={styles.card}>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur
-          quia eligendi modi debitis error, reiciendis itaque totam sit saepe
-          dignissimos rerum porro deserunt veniam fugit, eaque, voluptatum ipsum
-          aut maxime?
-        </li>
-        <li className={styles.card}>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit dolore
-          impedit eveniet molestiae quia inventore, consectetur obcaecati, non
-          iste quod ullam? Necessitatibus ut, natus velit in non ex eligendi
-          repellendus?
-        </li>
-        <li className={styles.card}>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut
-          dignissimos est impedit totam minima deleniti rerum voluptatibus eum
-          corrupti. Quia facilis sapiente at quaerat impedit repellendus in,
-          adipisci culpa eos?
-        </li>
-      </ul>
-    </div>
-  );
+const NUM_SLIDES = 10;
+const UNSPLASH = "https://source.unsplash.com/random";
+
+export class Slider extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      slides: Array.from({ length: NUM_SLIDES }).map((_, idx) => ({
+        image: `${UNSPLASH}?${idx}`
+      }))
+    };
+  }
+  render() {
+    const { slides } = this.state;
+    return (
+      <div className={styles.container}>
+        <div className={styles.overflow}>
+          <ul className={styles.slider}>
+            {slides.map(slide => (
+              <li
+                key={slide.image}
+                className={styles.slide}
+              >
+                <div style={{ backgroundImage: `url(${slide.image})` }} />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Slider;
+export default Slider
