d5b569eb737fef693ccbb5c917a42eb4f5dc2cc1
Dan Morgan <damorgan@hugeinc.com>
2019-05-09 12:49:56 -0400

    Add observer grid demo

--- src/scroll-reveal/index.jsx
+++ src/scroll-reveal/index.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { useInView } from "react-intersection-observer";
 
+const DEMO_BOXES = 64;
+const GRID_COLS = 6;
+const GRID_GAP = 24;
+
 export default function View() {
   return (
     <div>
@@ -13,107 +17,75 @@ export default function View() {
       </p>
       <p>react-intersection-observer</p>
 
-      <ul>
-        <VisibleListItem>Anime.js</VisibleListItem>
-        <VisibleListItem>CSS Modules</VisibleListItem>
-        <VisibleListItem>react-with-gesture?</VisibleListItem>
-        <VisibleListItem>Anime.js</VisibleListItem>
-        <VisibleListItem>CSS Modules</VisibleListItem>
-        <VisibleListItem>react-with-gesture?</VisibleListItem>
-        <VisibleListItem>Anime.js</VisibleListItem>
-        <VisibleListItem>CSS Modules</VisibleListItem>
-        <VisibleListItem>react-with-gesture?</VisibleListItem>
-        <VisibleListItem>Anime.js</VisibleListItem>
-        <VisibleListItem>CSS Modules</VisibleListItem>
-        <VisibleListItem>react-with-gesture?</VisibleListItem>
-        <VisibleListItem>Anime.js</VisibleListItem>
-        <VisibleListItem>CSS Modules</VisibleListItem>
-        <VisibleListItem>react-with-gesture?</VisibleListItem>
-        <VisibleListItem>Anime.js</VisibleListItem>
-        <VisibleListItem>CSS Modules</VisibleListItem>
-        <VisibleListItem>react-with-gesture?</VisibleListItem>
-        <VisibleListItem>Anime.js</VisibleListItem>
-        <VisibleListItem>CSS Modules</VisibleListItem>
-        <VisibleListItem>react-with-gesture?</VisibleListItem>
-        <VisibleListItem>Anime.js</VisibleListItem>
-        <VisibleListItem>CSS Modules</VisibleListItem>
-        <VisibleListItem>react-with-gesture?</VisibleListItem>
-        <VisibleListItem>Anime.js</VisibleListItem>
-        <VisibleListItem>CSS Modules</VisibleListItem>
-        <VisibleListItem>react-with-gesture?</VisibleListItem>
-        <VisibleListItem>Anime.js</VisibleListItem>
-        <VisibleListItem>CSS Modules</VisibleListItem>
-        <VisibleListItem>react-with-gesture?</VisibleListItem>
-        <VisibleListItem>Anime.js</VisibleListItem>
-        <VisibleListItem>CSS Modules</VisibleListItem>
-        <VisibleListItem>react-with-gesture?</VisibleListItem>
-        <VisibleListItem>Anime.js</VisibleListItem>
-        <VisibleListItem>CSS Modules</VisibleListItem>
-        <VisibleListItem>react-with-gesture?</VisibleListItem>
-        <VisibleListItem>Anime.js</VisibleListItem>
-        <VisibleListItem>CSS Modules</VisibleListItem>
-        <VisibleListItem>react-with-gesture?</VisibleListItem>
-        <VisibleListItem>Anime.js</VisibleListItem>
-        <VisibleListItem>CSS Modules</VisibleListItem>
-        <VisibleListItem>react-with-gesture?</VisibleListItem>
-        <VisibleListItem>Anime.js</VisibleListItem>
-        <VisibleListItem>CSS Modules</VisibleListItem>
-        <VisibleListItem>react-with-gesture?</VisibleListItem>
-        <VisibleListItem>Anime.js</VisibleListItem>
-        <VisibleListItem>CSS Modules</VisibleListItem>
-        <VisibleListItem>react-with-gesture?</VisibleListItem>
-        <VisibleListItem>Anime.js</VisibleListItem>
-        <VisibleListItem>CSS Modules</VisibleListItem>
-        <VisibleListItem>react-with-gesture?</VisibleListItem>
-        <VisibleListItem>Anime.js</VisibleListItem>
-        <VisibleListItem>CSS Modules</VisibleListItem>
-        <VisibleListItem>react-with-gesture?</VisibleListItem>
-        <VisibleListItem>Anime.js</VisibleListItem>
-        <VisibleListItem>CSS Modules</VisibleListItem>
-        <VisibleListItem>react-with-gesture?</VisibleListItem>
-        <VisibleListItem>Anime.js</VisibleListItem>
-        <VisibleListItem>CSS Modules</VisibleListItem>
-        <VisibleListItem>react-with-gesture?</VisibleListItem>
-        <VisibleListItem>Anime.js</VisibleListItem>
-        <VisibleListItem>CSS Modules</VisibleListItem>
-        <VisibleListItem>react-with-gesture?</VisibleListItem>
-        <VisibleListItem>Anime.js</VisibleListItem>
-        <VisibleListItem>CSS Modules</VisibleListItem>
-        <VisibleListItem>react-with-gesture?</VisibleListItem>
-        <VisibleListItem>Anime.js</VisibleListItem>
-        <VisibleListItem>CSS Modules</VisibleListItem>
-        <VisibleListItem>react-with-gesture?</VisibleListItem>
-        <VisibleListItem>Anime.js</VisibleListItem>
-        <VisibleListItem>CSS Modules</VisibleListItem>
-        <VisibleListItem>react-with-gesture?</VisibleListItem>
-        <VisibleListItem>Anime.js</VisibleListItem>
-        <VisibleListItem>CSS Modules</VisibleListItem>
-        <VisibleListItem>react-with-gesture?</VisibleListItem>
-        <VisibleListItem>Anime.js</VisibleListItem>
-        <VisibleListItem>CSS Modules</VisibleListItem>
-        <VisibleListItem>react-with-gesture?</VisibleListItem>
-        <VisibleListItem>Anime.js</VisibleListItem>
-        <VisibleListItem>CSS Modules</VisibleListItem>
-        <VisibleListItem>react-with-gesture?</VisibleListItem>
+      <ul
+        style={getListStyle()}
+      >
+        {Array.from({ length: DEMO_BOXES }).map((_, idx) => (
+          <VisibleListItemOnce key={idx}>{idx}</VisibleListItemOnce>
+        ))}
+      </ul>
+
+      <ul
+        style={getListStyle()}
+      >
+        {Array.from({ length: DEMO_BOXES }).map((_, idx) => (
+          <VisibleListItem key={idx}>{idx}</VisibleListItem>
+        ))}
       </ul>
     </div>
   );
 }
 
-function VisibleListItem({ children }) {
-  const [ref, inView] = useInView({ threshold: 0 });
+const DEFAULT_OPTIONS = { threshold: 0 };
+
+function VisibleListItem({ children, opts = DEFAULT_OPTIONS }) {
+  const [ref, inView] = useInView(opts);
   return (
-    <li
-      ref={ref}
-      style={{
-        margin: 0,
-        padding: '12px 24px',
-        opacity: inView ? 1 : 0,
-        transform: `translate3d(${inView ? 0 : -20 + 'px'}, 0, 0)`,
-        transition: "all 400ms ease"
-      }}
-    >
+    <li ref={ref} style={getItemStyle(inView)}>
       {children}
     </li>
   );
+}
+
+function VisibleListItemOnce({ children, opts = {} }) {
+  const [ref, inView] = useInView({
+    ...DEFAULT_OPTIONS,
+    opts,
+    triggerOnce: true
+  });
+  return (
+    <li ref={ref} style={getItemStyle(inView)}>
+      {children} (once)
+    </li>
+  );
+}
+
+function getListStyle(){
+  return {
+    listStyle: "none",
+    display: "grid",
+    gridTemplateRows: "auto",
+    gridAutoRows: "auto",
+    gridRowGap: GRID_GAP,
+    gridColumnGap: GRID_GAP,
+    gridTemplateColumns: `repeat(${GRID_COLS}, 1fr)`
+  }
+}
+
+function getItemStyle(inView) {
+  return {
+    margin: 0,
+    padding: "12px 24px",
+    height: 200,
+    backgroundColor: "#eee",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
+    opacity: inView ? 1 : 0,
+    transform: `translate3d(0, ${inView ? 0 : -20 + "px"}, 0) scale(${
+      inView ? 1 : 0.8
+    })`,
+    transition: "all 400ms ease"
+  };
 }
