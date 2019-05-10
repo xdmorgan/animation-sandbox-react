638fe43bffa463acf386a46470356639e0571bd4
Dan Morgan <dannydev@icloud.com>
2019-05-10 17:01:29 -0400

    Feature/intersection observer (#4)
    * Install scroll reveal animation dependencies
    * Add observer grid demo
    * HSL intersection observer demo

--- src/scroll-reveal/index.jsx
+++ src/scroll-reveal/index.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { useInView } from "react-intersection-observer";
+// import cx from 'classnames';
+import styles from './scroll.module.scss';
 
-const DEMO_BOXES = 64;
-const GRID_COLS = 6;
-const GRID_GAP = 24;
+const DEMO_BOXES = 255;
 
 export default function View() {
   return (
@@ -16,76 +16,37 @@ export default function View() {
         https://www.npmjs.com/package/intersection-observer#configuring-the-polyfill
       </p>
       <p>react-intersection-observer</p>
-
-      <ul
-        style={getListStyle()}
-      >
-        {Array.from({ length: DEMO_BOXES }).map((_, idx) => (
-          <VisibleListItemOnce key={idx}>{idx}</VisibleListItemOnce>
-        ))}
-      </ul>
-
-      <ul
-        style={getListStyle()}
-      >
-        {Array.from({ length: DEMO_BOXES }).map((_, idx) => (
-          <VisibleListItem key={idx}>{idx}</VisibleListItem>
-        ))}
-      </ul>
+      <div className={styles.container}>
+        <ul className={styles.list}>
+          {Array.from({ length: DEMO_BOXES }).map((_, idx) => (
+            <VisibleListItem key={idx}>{idx}</VisibleListItem>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
 
-const DEFAULT_OPTIONS = { threshold: 0 };
+const DEFAULT_OPTIONS = { threshold: 0.1 };
 
-function VisibleListItem({ children, opts = DEFAULT_OPTIONS }) {
-  const [ref, inView] = useInView(opts);
-  return (
-    <li ref={ref} style={getItemStyle(inView)}>
-      {children}
-    </li>
-  );
-}
+// function VisibleListItem({ children, opts = DEFAULT_OPTIONS }) {
+//   const [ref, inView] = useInView(opts);
+//   return (
+//     <li ref={ref} className={`${styles.item} ${inView ? '' : styles.offscreen}`}>
+//       {children}
+//     </li>
+//   );
+// }
 
-function VisibleListItemOnce({ children, opts = {} }) {
+function VisibleListItem({ children, opts = {} }) {
   const [ref, inView] = useInView({
     ...DEFAULT_OPTIONS,
     opts,
-    triggerOnce: true
+    // triggerOnce: true
   });
   return (
-    <li ref={ref} style={getItemStyle(inView)}>
-      {children} (once)
+    <li ref={ref} className={`${styles.item} ${inView ? '' : styles.offscreen}`}>
+      {children}
     </li>
   );
-}
-
-function getListStyle(){
-  return {
-    listStyle: "none",
-    display: "grid",
-    gridTemplateRows: "auto",
-    gridAutoRows: "auto",
-    gridRowGap: GRID_GAP,
-    gridColumnGap: GRID_GAP,
-    gridTemplateColumns: `repeat(${GRID_COLS}, 1fr)`
-  }
-}
-
-function getItemStyle(inView) {
-  return {
-    margin: 0,
-    padding: "12px 24px",
-    height: 200,
-    backgroundColor: "#eee",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    textAlign: "center",
-    opacity: inView ? 1 : 0,
-    transform: `translate3d(0, ${inView ? 0 : -20 + "px"}, 0) scale(${
-      inView ? 1 : 0.8
-    })`,
-    transition: "all 400ms ease"
-  };
 }
